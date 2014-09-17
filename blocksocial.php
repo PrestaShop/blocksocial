8394691162ea1d67b31cd43d6154f021fcfd4274
Jérôme Nadaud <jerome.nadaud@prestashop.com>
2014-09-17 11:56:41 +0200

    // Version updated to v1.1.5

--- blocksocial.php
+++ blocksocial.php
@@ -33,7 +33,7 @@ class blocksocial extends Module
 	{
 		$this->name = 'blocksocial';
 		$this->tab = 'front_office_features';
-		$this->version = '1.1.4';
+		$this->version = '1.1.5';
 		$this->author = 'PrestaShop';
 
 		$this->bootstrap = true;
