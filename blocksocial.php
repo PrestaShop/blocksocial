5650c04c53bc0bcb0148f22823306c695a3e190a
Thomas N <thomas.nabord@prestashop.com>
2015-08-20 14:53:00 +0200

    // Version updated to v1.2.1

--- blocksocial.php
+++ blocksocial.php
@@ -33,7 +33,7 @@ class blocksocial extends Module
 	{
 		$this->name = 'blocksocial';
 		$this->tab = 'front_office_features';
-		$this->version = '1.2.0';
+		$this->version = '1.2.1';
 		$this->author = 'PrestaShop';
 
 		$this->bootstrap = true;
